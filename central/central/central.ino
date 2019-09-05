71283a56666215f63d73f0875cbb214d33f0236c
jorge <jorge.meza.rdz@gmail.com>
2019-09-04 22:37:43 -0500

    inicio

--- central/central/central.ino
+++ central/central/central.ino
@@ -1,9 +1,63 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include <RF24.h>
+RF24 radio(7, 8); // CE, CSN
 
+/* Creamos una matriz de bytes que represente la 
+direccion con la que se reconoceran entre si los 
+dos modulos. Podemos cambiar el valor de esta 
+direccion a cualquier conjunto de 5 letras y/o 
+numeros. Esta direccion permite elegir con que 
+receptor vamos a comunicarnos. En nuestro caso, 
+usaremos la misma direccion tanto en el receptor 
+como en el transmisor. */
+const byte identificacion[6] = "00001";
+
+void setup() {
+  Serial.begin(9600); // Iniciamos la comunicacion serie hacia el Monitor Serie en la PC
+
+/* En la sección de configuracion debemos 
+inicializar el objeto “radio” */
+  radio.begin();
+
+/* En el receptor establecemos la misma direccion 
+que en el transmisor usando la funcion 
+radio.setReadingPipe() y asi queda habilitada 
+la comunicacion entre los dos modulos. */
+  radio.openReadingPipe(0, identificacion);
+
+/* Configuramos el nivel del amplificador de potencia,
+usando la funcion radio.setPALevel(). Mientras 
+estemos a este nivel de prueba, es decir prototipos 
+sobre la mesa de trabajo, lo establecemos al minimo, 
+ya que los modulos estaran cerca el uno del otro. 
+Si se utiliza un nivel mas alto, se recomienda 
+utilizar capacitores de filtro entre las entradas 
+GND y 3,3 V de los mdulos para que su voltaje se 
+mantenga mas estable durante el funcionamiento. */
+  radio.setPALevel(RF24_PA_MIN);
+
+/* La funcion radio.startListening() establece al 
+modulo como receptor. */
+  radio.startListening();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (radio.available()) {
+  char texto[32] = ""; 
+/* Creamos una matriz de caracteres donde recibir el 
+mensaje */
 
+  radio.read(&texto, sizeof(texto)); // Esperamos que llegue algo por RF desde el módulo
+/* El símbolo “&” delante del nombre de la variable 
+establece un indicador que señala a la variable que 
+contiene los datos que queremos enviar y, con el 
+segundo argumento, establecemos la cantidad de bytes 
+que se van a usar de esa variable. En este caso, la 
+funcion sizeof() nos devolvera la cantidad total de 
+los bytes de la cadena "texto". */
+
+  Serial.println(texto);
+/* Enviamos lo que hemos recibido al Monitor 
+Serie en la PC */
+ }
 }
--- pozo1/pozo1.ino
+++ pozo1/pozo1.ino
@@ -1,9 +1,79 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include <RF24.h>
 
+RF24 radio(7, 8); // CE, CSN
+const byte identificacion[6] = "00001";
+
+const int caudal1 = 2;
+const int caudal2 = 3;
+
+volatile int countCaudal1 = 0;
+volatile int countCaudal2 = 0;
+
+volatile unsigned long presenteMillis = 0;
+volatile unsigned long pasadoMillis = 0;
+
+volatile int intervaloCaudal = 180;
+volatile int segundosCaudal = 0;
+volatile int litrosPulso1 = 100;
+volatile int litrosPulso2 = 100;
+volatile int valCaudal1 = 0;
+volatile int valCaudal2 = 0;
+
+
+
+void setup() {
+
+  pinMode(caudal1, INPUT_PULLUP);
+  pinMode(caudal2, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(caudal1), isr_caudal1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(caudal2), isr_caudal2, FALLING);
+
+  radio.begin();
+  radio.openWritingPipe(identificacion);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+  pasadoMillis = millis();
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+
+
+
+  presenteMillis =  millis();
+  if (presenteMillis > (pasadoMillis + 1000))
+  {
+    segundosCaudal ++;
+
+    if (segundosCaudal == intervaloCaudal)
+    {
+      segundosCaudal = 0;
+      calculoCaudal();
+
+    }
+   const char texto[] = "Pozo 1";
+   radio.write(&texto, sizeof(texto));
+  
+    pasadoMillis = millis();
+  }
+
+}
+
+void isr_caudal1() {
+  countCaudal1 ++;
+}
+
+void isr_caudal2() {
+  countCaudal2 ++;
+}
+
+void calculoCaudal() {
+
+valCaudal1=(countCaudal1*litrosPulso1)/intervaloCaudal;
+valCaudal2=(countCaudal2*litrosPulso2)/intervaloCaudal;
 
 }
