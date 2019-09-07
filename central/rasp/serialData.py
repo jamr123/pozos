f9d10770227991155fcc318d7586de57b26fd346
jorge <jorge.meza.rdz@gmail.com>
2019-09-07 15:51:20 -0500

    modoficacion

--- central/rasp/serialData.py
+++ central/rasp/serialData.py
@@ -4,8 +4,8 @@ import threading
 
 # configure the serial connections (the parameters differs on the device you are connecting to)
 #ser =serial.Serial('/dev/ttyAMA0', 115200, timeout=1)
-ser =serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-
+#ser =serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+ser =serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
 #http=httpRequest.Http()
 class Data:
