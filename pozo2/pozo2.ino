bc5bf691c5ef0fe9f528cb35d633f13067eb5234
jorge <jorge.meza.rdz@gmail.com>
2019-09-08 15:09:10 -0500

    actualizacion 3 pozos

--- pozo2/pozo2.ino
+++ pozo2/pozo2.ino
@@ -1,17 +1,24 @@
+
 #include <SPI.h>
 #include <RF24.h>
 
 RF24 radio(9, 10); // CE, CSN
 const byte identificacion[6] = "00001";
 
+#define SELPIN 8 //Selection Pin 
+#define DATAOUT 5//MOSI 
+#define DATAIN  6//MISO 
+#define SPICLOCK  7//Clock 
+int readvalue;
+
+volatile unsigned long presenteMillis = 0;
+volatile unsigned long pasadoMillis = 0;
+
 const int caudal1 = 2;
 const int caudal2 = 3;
 
 volatile int countCaudal1 = 0;
 volatile int countCaudal2 = 0;
-
-volatile unsigned long presenteMillis = 0;
-volatile unsigned long pasadoMillis = 0;
 
 volatile int intervaloCaudal = 180;
 volatile int segundosCaudal = 0;
@@ -21,66 +28,100 @@ volatile int valCaudal1 = 0;
 volatile int valCaudal2 = 0;
 
 
-#define SELPIN 8 //Selection Pin 
-#define DATAOUT 5//MOSI 
-#define DATAIN  6//MISO 
-#define SPICLOCK  7//Clock
-
-int readA1 = 0;
-int readA2 = 0;
-int readA3 = 0;
-int readA4 = 0;
-int inA1 = 0;
-int inA2 = 0;
-int inA3 = 0;
-int inA4 = 0;
-String sendData = "";
-
-
 
 void setup() {
-
   pinMode(caudal1, INPUT_PULLUP);
   pinMode(caudal2, INPUT_PULLUP);
 
   attachInterrupt(digitalPinToInterrupt(caudal1), isr_caudal1, FALLING);
   attachInterrupt(digitalPinToInterrupt(caudal2), isr_caudal2, FALLING);
-
+  
+  //set pin modes
+  pinMode(SELPIN, OUTPUT);
+  pinMode(DATAOUT, OUTPUT);
+  pinMode(DATAIN, INPUT);
+  pinMode(SPICLOCK, OUTPUT);
+  //disable device to start with
+  digitalWrite(SELPIN, HIGH);
+  digitalWrite(DATAOUT, LOW);
+  digitalWrite(SPICLOCK, LOW);
+  
   radio.begin();
   radio.openWritingPipe(identificacion);
   radio.setPALevel(RF24_PA_MAX);
   radio.stopListening();
-  pasadoMillis = millis();
-
-
 
   Serial.begin(9600);
+  pasadoMillis = millis();
 }
+
+int read_adc(int channel) {
+  int adcvalue = 0;
+  byte commandbits = B11000000; //command bits - start, mode, chn (3), dont care (3)
+
+  //allow channel selection
+  commandbits |= ((channel - 1) << 3);
+
+  digitalWrite(SELPIN, LOW); //Select adc
+  // setup bits to be written
+  for (int i = 7; i >= 3; i--) {
+    digitalWrite(DATAOUT, commandbits & 1 << i);
+    //cycle clock
+    digitalWrite(SPICLOCK, HIGH);
+    digitalWrite(SPICLOCK, LOW);
+  }
+
+  digitalWrite(SPICLOCK, HIGH);   //ignores 2 null bits
+  digitalWrite(SPICLOCK, LOW);
+  digitalWrite(SPICLOCK, HIGH);
+  digitalWrite(SPICLOCK, LOW);
+
+  //read bits from adc
+  for (int i = 11; i >= 0; i--) {
+    adcvalue += digitalRead(DATAIN) << i;
+    //cycle clock
+    digitalWrite(SPICLOCK, HIGH);
+    digitalWrite(SPICLOCK, LOW);
+  }
+  digitalWrite(SELPIN, HIGH); //turn off device
+  return adcvalue;
+}
+
 
 void loop() {
 
   presenteMillis =  millis();
+
+
   if (presenteMillis > (pasadoMillis + 1000))
   {
-    segundosCaudal ++;
-
-    if (segundosCaudal == intervaloCaudal)
-    {
-      segundosCaudal = 0;
-      calculoCaudal();
-
-    }
-
-    Serial.println("");
-    Serial.println(countCaudal1);
-    Serial.println(countCaudal2);
-    Serial.println("");
-    enviarData();
-
+     enviarData(read_adc(1),read_adc(2),read_adc(3),read_adc(4),valCaudal1,valCaudal2);
     pasadoMillis = millis();
   }
 
-  readTarjeta();
+
+
+
+}
+
+void enviarData(int adc1, int adc2 ,int adc3, int adc4, int cdl1, int cdl2)
+{
+  String sendDato = "POZO2";
+   sendDato =  sendDato + "$" + String(adc1);
+   sendDato =  sendDato + "$" + String(adc2);
+   sendDato =  sendDato + "$" + String(adc3);
+   sendDato =  sendDato + "$" + String(adc4);
+   sendDato =  sendDato + "$" + String(cdl1);
+   sendDato =  sendDato + "$" + String(cdl2);
+
+
+  Serial.println(sendDato);
+
+  int str_len =  sendDato.length() + 1;
+  char data[str_len];
+  sendDato.toCharArray(data, str_len);
+  radio.write(&data, sizeof(data));
+
 
 }
 
@@ -99,71 +140,4 @@ void calculoCaudal() {
   countCaudal1 = 0;
   countCaudal2 = 0;
 
-}
-
-int read_adc(int channel) {
-  int adcvalue = 0;
-  byte commandbits = B11000000; 
-  commandbits |= ((channel - 1) << 3);
-
-  digitalWrite(SELPIN, LOW);
-  for (int i = 7; i >= 3; i--) {
-    digitalWrite(DATAOUT, commandbits & 1 << i);
-    digitalWrite(SPICLOCK, HIGH);
-    digitalWrite(SPICLOCK, LOW);
-  }
-
-  digitalWrite(SPICLOCK, HIGH);   
-  digitalWrite(SPICLOCK, LOW);
-  digitalWrite(SPICLOCK, HIGH);
-  digitalWrite(SPICLOCK, LOW);
-
-  for (int i = 11; i >= 0; i--) {
-    adcvalue += digitalRead(DATAIN) << i;
-    digitalWrite(SPICLOCK, HIGH);
-    digitalWrite(SPICLOCK, LOW);
-  }
-  digitalWrite(SELPIN, HIGH); 
-  return adcvalue;
-}
-
-void enviarData()
-{
-  sendData = "POZO2";
-  sendData = sendData + "$" + String(readA1);
-  sendData = sendData + "$" + String(readA2);
-  sendData = sendData + "$" + String(readA3);
-  sendData = sendData + "$" + String(readA4);
-  sendData = sendData + "$" + String(valCaudal1);
-  sendData = sendData + "$" + String(valCaudal2);
-
-
-  Serial.println(sendData);
-
-  int str_len =  sendData.length() + 1;
-  char data[str_len];
-  sendData.toCharArray(data, str_len);
-  radio.write(&data, sizeof(data));
-
-
-}
-
-void readTarjeta() {
-  inA1 = read_adc(1);
-  inA2 = read_adc(2);
-  inA3 = read_adc(3);
-  inA4 = read_adc(4);
-
-  if (inA1 > 0) {
-    readA1 = A1;
-  }
-  if (inA2 > 0) {
-    readA2 = A2;
-  }
-  if (inA3 > 0) {
-    readA3 = A3;
-  }
-  if (inA4 > 0) {
-    readA4 = A4;
-  }
 }
