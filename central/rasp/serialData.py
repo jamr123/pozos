4a225c63f870893f46bebb5ad2e742a06873c7b0
jorge <jorge.meza.rdz@gmail.com>
2019-09-07 20:43:02 -0500

    modoficacion

--- central/rasp/serialData.py
+++ central/rasp/serialData.py
@@ -38,23 +38,43 @@ class Data:
         puntero=data[0]
         if puntero =="POZO1":
             print("data pozo1")
+            nivel1=int(data[1])
+            print(nivel1)
 
         elif puntero =="POZO2":
-            print("data pozo1")
+            print("data pozo2")
 
         elif puntero =="POZO3":
-            print("data pozo1")
+            print("data pozo3")
 
         elif puntero =="TEMP1":
-            print("data pozo1")
+            print("data T1")
 
         elif puntero =="TEMP2":
-            print("data pozo1")
+            print("data T2")
 
         elif puntero =="TEMP3":
-            print("data pozo1")
+            print("data T3")
    
-
+    def valorInstrumentacion(self,data):
+        sen=data[0]
+        ri_min=data[1]
+        ri_max=data[2]
+        rp_min=data[3]
+        rp_max=data[4]
+        try:
+            x=self.readInputCorriente(sen)
+            
+            
+            rangoInstrumentacion=ri_max-ri_min
+            rangoProceso=rp_max-rp_min
+            rirp=rangoInstrumentacion/rangoProceso
+            val=((ri_max-x)/rirp)-rp_max
+            val=val*-1
+            return val
+            
+        except Exception as e:
+            return 0
     
 
 
